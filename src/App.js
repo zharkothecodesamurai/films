e98d35d7f4a0220b8408dd6a73c9414989f726dd
Zharko Naumovski <zharko.naumovski@seavus.com>
2022-02-18 00:37:36 +0100

    user log functionality basic and custom hook

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import { Fragment } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from 'react-router-dom';
 
+import Login from "./components/Login/Login";
 import Header from "./components/Layout/Header";
 import Movies from "./components/Movies/Movies";
 
@@ -7,10 +16,17 @@ import Movies from "./components/Movies/Movies";
 function App() {
   return (
     <Fragment>
-      <Header />
-      <main>
-        <Movies/>
-      </main>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<main>
+            <Movies />
+          </main>}>
+          </Route>
+          <Route path="/login" element={<Login />}>
+          </Route>
+        </Routes>
+      </Router>
     </Fragment>
   );
 }
--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,17 +1,21 @@
 import { Fragment } from "react";
 
-
+import { Link } from 'react-router-dom';
 import movienight from "../../assets/movienight.jpg"
 import classes from "./Header.module.css"
 
 
-const Header = props =>{
-return <Fragment>
+const Header = props => {
+    return <Fragment>
         <header className={classes.header}>
-            <h1>Movisimo</h1>           
+        <Link to={'/'} style={{ textDecoration: 'none', color:"white" }}><h1>Movisimo</h1></Link>
+            <Fragment><Link to={'/login'}>
+                <span className={classes.link}>Log In</span>
+            </Link>  </Fragment>
+
         </header>
         <div className={classes['main-image']}>
-            <img src={movienight} alt="a wall with text"/>
+            <img src={movienight} alt="a wall with text" />
         </div>
     </Fragment>;
 }
